a4711e9e7bdaad0a133db41f22fab3baf005d96f
Marina <reddymikks@gmail.noreply.github.com>
2023-04-25 16:35:05 +0300

    Add saving to local storage

--- src/toDoFunctions.js
+++ src/toDoFunctions.js
@@ -1,20 +1,11 @@
-export let todoList = [];
+import { demoTodoList } from "./demoTodoList";
 
-class Task {
-  constructor(title, description, priority, dueDate, isComplete) {
-    this.title = title;
-    this.description = description;
-    this.priority = priority;
-    this.dueDate = dueDate;
-    this.isComplete = isComplete;
-  }
-}
+const savedTodoList = JSON.parse(localStorage.getItem("todoList"));
 
-class Project {
-  constructor(title) {
-    this.title = title;
-    this.tasks = [];
-  }
+export let todoList = savedTodoList || demoTodoList;
+
+export function saveTodoList() {
+  localStorage.setItem("todoList", JSON.stringify(todoList));
 }
 
 export function addNewTask(
@@ -25,23 +16,27 @@ export function addNewTask(
   isComplete,
   project
 ) {
-  const task = new Task(title, description, priority, dueDate, isComplete);
+  const task = { title, description, priority, dueDate, isComplete };
   todoList[project].tasks.push(task);
+  saveTodoList();
 }
 
 export function addNewProject(title) {
-  const project = new Project(title);
+  const project = { title, tasks: [] };
   todoList.push(project);
+  saveTodoList();
 }
 
 export function deleteProject(project) {
   todoList = todoList.filter((projects) => projects !== todoList[project]);
+  saveTodoList();
 }
 
 export function deleteTask(project, task) {
   todoList[project].tasks = todoList[project].tasks.filter(
     (tasks) => tasks !== todoList[project].tasks[task]
   );
+  saveTodoList();
 }
 
 export function changeTaskStatus(project, task) {
@@ -51,10 +46,12 @@ export function changeTaskStatus(project, task) {
   } else {
     taskToStatusChange.isComplete = true;
   }
+  saveTodoList();
 }
 
 export function renameProject(project, newTitle) {
   todoList[project].title = newTitle;
+  saveTodoList();
 }
 
 export function editTask(
@@ -70,98 +67,5 @@ export function editTask(
   taskToEdit.description = newDescription;
   taskToEdit.priority = newPriority;
   taskToEdit.dueDate = newDueDate;
+  saveTodoList();
 }
-
-addNewProject("Work");
-addNewProject("Study");
-addNewProject("Home");
-addNewProject("Leisure");
-
-addNewTask("Check the mailbox", "Spam too", "Medium", "2023/04/30", false, 0);
-addNewTask(
-  "Apply project change",
-  "Check the new ones",
-  "High",
-  "2023/04/25",
-  false,
-  0
-);
-addNewTask("Call Julia", "Ask about the report", "High", "2023/04/22", true, 0);
-addNewTask(
-  "Make vacation plans",
-  "Call the manager",
-  "Low",
-  "2023/09/23",
-  false,
-  0
-);
-
-addNewTask(
-  "Finish the to-do project",
-  "Optimise code",
-  "High",
-  "2023/04/24",
-  false,
-  1
-);
-addNewTask(
-  "Read the past conspect",
-  "On classes",
-  "Medium",
-  "2023/04/24",
-  true,
-  1
-);
-addNewTask(
-  "Return to validation JS",
-  "Practice",
-  "Medium",
-  "2023/04/26",
-  false,
-  1
-);
-
-addNewTask("Do the laundry", "White", "High", "04/23", false, 2);
-addNewTask(
-  "Mice cleaning",
-  "Water bottle too",
-  "Medium",
-  "2023/04/22",
-  false,
-  2
-);
-addNewTask("Get the delivery", "20:00 - 22:00", "Low", "2023/04/22", true, 2);
-addNewTask(
-  "Collect clothes for charity",
-  "Check the points",
-  "Low",
-  "2023/05/25",
-  false,
-  2
-);
-addNewTask(
-  "Watch Loro film",
-  "On the TV screen",
-  "Medium",
-  "2023/05/01",
-  false,
-  3
-);
-addNewTask(
-  "Watch Napoleon film",
-  "Finish book first",
-  "High",
-  "2023/04/23",
-  false,
-  3
-);
-
-// deleteProject("3");
-// deleteTask(1, 0);
-// deleteTask(1, 0);
-// changeTaskStatus(0, 0);
-
-// console.log(todoList[0].tasks[0].isComplete);
-// console.log(todoList[0].tasks[0]);
-// console.log(todoList.length);
-// console.log(todoList[1].tasks.length);
